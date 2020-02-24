2155c1d01eaa5b75fd810833e78c7d1524c66242
Daiki Mizukami <tesaguriguma@gmail.com>
2020-02-25 03:40:31 +0900

    Handle verification requests (spec §5.3)

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,24 @@ struct Opt {
     addr: SocketAddr,
 }
 
+#[derive(serde::Deserialize, Default, Debug)]
+struct Verify {
+    #[serde(rename = "hub.mode")]
+    mode: String,
+    #[serde(rename = "hub.topic")]
+    topic: String,
+    #[serde(rename = "hub.challenge")]
+    challenge: String,
+    #[serde(rename = "hub.lease_seconds")]
+    lease_seconds: Option<String>,
+}
+
+#[derive(serde::Deserialize)]
+struct Maybe<T> {
+    #[serde(flatten)]
+    value: Option<T>,
+}
+
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
@@ -20,12 +38,24 @@ async fn main() {
         future::ok::<_, Error>(service_fn(|req: Request<Body>| {
             eprintln!("* {}", req.uri());
             let mut stdout = stdout();
-            req.into_body()
-                .try_for_each(move |chunk| {
-                    stdout.write_all(&chunk).unwrap();
-                    future::ok(())
-                })
-                .map_ok(|()| Response::new(Body::empty()))
+            async {
+                if let Some(q) = req.uri().query() {
+                    if let Some(hub) = serde_urlencoded::from_str::<Maybe<Verify>>(q)
+                        .unwrap()
+                        .value
+                    {
+                        // TODO: verify the request
+                        return Ok(Response::new(Body::from(hub.challenge)));
+                    }
+                }
+                req.into_body()
+                    .try_for_each(move |chunk| {
+                        stdout.write_all(&chunk).unwrap();
+                        future::ok(())
+                    })
+                    .map_ok(|()| Response::new(Body::empty()))
+                    .await
+            }
         }))
     });
 
